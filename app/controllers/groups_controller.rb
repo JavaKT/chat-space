b783868c2b53e892967c695d6170552685b2336e
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-10-28 20:02:42 +0900

    add group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,28 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
+
   def new
     @group = Group.new
     @group.users << current_user 
   end
 
   def create
-    @group = Group.new(gropu_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path,  notice:"グループを作成しました"
     else
       render :new
   end
+end
+
 
   private
 
   def group_params
-  params.require(:gruop).permit(:name, user_ids: [])
+  params.require(:group).permit(:name, user_ids: [])
   end
 
 
