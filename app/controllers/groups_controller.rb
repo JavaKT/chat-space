cd71bfa1f38568e4f48c8ad158be9e0bb4a87a68
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-10-29 10:53:28 +0900

    Change Color of h2 of user.html.haml

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -18,6 +19,11 @@ class GroupsController < ApplicationController
     end
 
   end
+
+  def edit
+    set_group
+  end
+
 
   def update
     if @group.update(group_params)
