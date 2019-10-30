b1a8a2993d433bcede8cb42ba47a61cff54b3fff
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-10-30 17:19:58 +0900

    finish

--- db/migrate/20191028065955_create_group_users.rb
+++ db/migrate/20191028065955_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.references :group, foregin_key: true
-      t.references :user, foregin_key: true
+      t.references :group, foregin_key: true, unique: true
+      t.references :user, foregin_key: true, unique: true
       t.timestamps 
     end
   end
