68fc739f9951341268df1f25e1432a0306f55b86
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-10-30 15:55:55 +0900

    view totonoeru

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,13 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  #Include RMagick or MiniMagick support:
+  # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_fit: [100, 100]
 
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  #storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -30,10 +30,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-   version :thumb do
-     process resize_to_fit: [800, 800]
-   end
+  #Create different versions of your uploaded files:
+  #version :thumb do
+  #process resize_to_fit: [800, 800]
+  #end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
