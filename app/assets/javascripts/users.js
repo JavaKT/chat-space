9418bd6d1fe9e4ff7f962f072fa25133c1bf2872
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-11-05 19:57:28 +0900

    edti html

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,10 @@
 $(function(){
   function  addUser(user){
+
     let html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= ${user.id} data-user-name="${user.name}">追加</a>
               </div> `
     $("#user-search-result").append(html);
   }
@@ -17,15 +18,19 @@ $(function(){
     $("#user-search-result").append(html);
 }
 
-function addDeleteUser(name,id) {
-  let html = `<div class="ChatMember clearfix" id="${id}">
-              <p class="ChatMember__name">${name}</p>
-                <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
-              </div>`;
-    $(".ChatMembers")
-
+function addDeleteUser(id,name) {
+  var html = `<div class="chat-group-user clearfix" id="${id}">
+                <p class="chat-group-user__name">${name}</p>
+                <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+              </div>`     
+  $("#chat-group-users.js-add-user").append(html)
 }
 
+function addMember(id){
+let html = `<input value="${id}" name="group[user_ids][]" type="hidden" id="group_user_ids_${id}" />`;
+              $(`#${id}`).append(html);
+
+}
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -34,7 +39,7 @@ function addDeleteUser(name,id) {
   $.ajax({
     type: "GET",    
     url: "/users",       
-    dataType: 'json',
+    dataType:'json',
     data: {keyword : input},
    })
 
@@ -59,5 +64,15 @@ function addDeleteUser(name,id) {
 
    })
   })
+  $(document).on("click",".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function(){
+    var id = $(this).attr("data-user-id")
+    var name = $(this).attr("data-user-name")
+    $(this).parent().remove();
+    addDeleteUser(id,name);
+    addMember(id);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this).parent().remove();
+  });
 })
 
